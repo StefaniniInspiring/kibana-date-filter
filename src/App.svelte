50aaccbd91ae6aecf3d127c7ee8611f38ccc3445
leonavas <rodrigoleonavas@gmail.com>
2021-01-08 12:36:29 -0300

    ajuste no caso de url invalida

--- src/App.svelte
+++ src/App.svelte
@@ -11,43 +11,45 @@
 
   onMount(async () => {
     document.addEventListener('DOMContentLoaded', function () {
-      picker = new Litepicker({
-        element: document.getElementById('litepicker'),
-        singleMode: false,
-        format: 'DD/MM/YYYY',
-        numberOfColumns: 2,
-        numberOfMonths: 2,
-        lang: 'pt-BR',
-        tooltipText: {
-          one: 'dia',
-          other: 'dias',
-        },
-        useResetBtn: true,
-        moveByOneMonth: true,
-        moduleRanges: {
-          position: 'left',
-          ranges: { Hoje: [toDayBeginnig(new Date()), toDayEnd(new Date())] },
-        },
-        mobileFriendly: true,
-        autoApply: true,
-        allowRepick: true,
-        buttonText: {
-          apply: 'OK',
-          cancel: 'Cancelar',
-          reset: `<span style="display: flex;">Limpar</span> <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+      if (src != null) {
+        picker = new Litepicker({
+          element: document.getElementById('litepicker'),
+          singleMode: false,
+          format: 'DD/MM/YYYY',
+          numberOfColumns: 2,
+          numberOfMonths: 2,
+          lang: 'pt-BR',
+          tooltipText: {
+            one: 'dia',
+            other: 'dias',
+          },
+          useResetBtn: true,
+          moveByOneMonth: true,
+          moduleRanges: {
+            position: 'left',
+            ranges: { Hoje: [toDayBeginnig(new Date()), toDayEnd(new Date())] },
+          },
+          mobileFriendly: true,
+          autoApply: true,
+          allowRepick: true,
+          buttonText: {
+            apply: 'OK',
+            cancel: 'Cancelar',
+            reset: `<span style="display: flex;">Limpar</span> <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
         <path d="M0 0h24v24H0z" fill="none"></path>
         <path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"></path>
       </svg>`,
-        },
-        resetBtnCallback: function () {
-          src = defaultSrc
-          picker.hide()
-        },
-        onSelect: function (date1, date2) {
-          picker.hide()
-          changePeriod(date1, toDayEnd(date2))
-        },
-      })
+          },
+          resetBtnCallback: function () {
+            src = defaultSrc
+            picker.hide()
+          },
+          onSelect: function (date1, date2) {
+            picker.hide()
+            changePeriod(date1, toDayEnd(date2))
+          },
+        })
+      }
     })
   })
 
@@ -59,7 +61,7 @@
   }
 
   function toDayEnd(date) {
-    date.setHours(23,59,59,999);
+    date.setHours(23, 59, 59, 999)
     return date
   }
 
@@ -102,10 +104,9 @@
 
   hideFilters()
 
-  src = decodeURI(findUrlParam('url'))
-  console.log(src)
-  defaultSrc = src
+  src = findUrlParam('url')
 
+  defaultSrc = src
 </script>
 
 <style>
